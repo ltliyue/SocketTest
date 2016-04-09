d0b8e569efffe992ca477c674d4342c3f88685c6
ltliyue <ltliyue@qq.com>
2016-04-09 15:06:24 +0800

    socket通信
    client server

--- src/com/example/socket/Client2.java
+++ src/com/example/socket/Client2.java
@@ -8,12 +8,9 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.concurrent.ConcurrentHashMap;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.widget.TextView;
 
 /**
  * C/S架构的客户端对象，持有该对象，可以随时向服务端发送消息。
@@ -24,142 +21,144 @@ import android.widget.TextView;
  * @since 1.0
  */
 public class Client2 {
-    Handler mHandler;
-    private String serverIp;
-    private int port;
-    public Socket socket;
-    private boolean running = false;
-    private long lastSendTime;
-    private ConcurrentHashMap<Class, ObjectAction> actionMapping = new ConcurrentHashMap<Class, ObjectAction>();
+	Handler mHandler;
+	private String serverIp;
+	private int port;
+	public Socket socket;
+	private boolean running = false;
+	private long lastSendTime;
+	private ConcurrentHashMap<Class, ObjectAction> actionMapping = new ConcurrentHashMap<Class, ObjectAction>();
 
-    public Client2(Handler mHandler, String serverIp, int port) {
-        this.mHandler = mHandler;
-        this.serverIp = serverIp;
-        this.port = port;
-    }
+	public Client2(Handler mHandler, String serverIp, int port) {
+		this.mHandler = mHandler;
+		this.serverIp = serverIp;
+		this.port = port;
+	}
 
-    public void start() throws UnknownHostException, IOException {
-        if (running)
-            return;
-        Looper.prepare();
-        try {
-            socket = new Socket(serverIp, port);
-            System.out.println("本地端口：" + socket.getLocalPort());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	public void start() throws UnknownHostException, IOException {
+		if (running)
+			return;
+		Looper.prepare();
+		try {
+			socket = new Socket(serverIp, port);
+			System.out.println("本地端口：" + socket.getLocalPort());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        lastSendTime = System.currentTimeMillis();
-        running = true;
-        new Thread(new KeepAliveWatchDog()).start();
-        new Thread(new ReceiveWatchDog()).start();
-    }
+		lastSendTime = System.currentTimeMillis();
+		running = true;
+		new Thread(new KeepAliveWatchDog()).start();
+		new Thread(new ReceiveWatchDog()).start();
+	}
 
-    public void stop() {
-        if (running)
-            running = false;
-    }
+	public void stop() {
+		if (running)
+			running = false;
+	}
 
-    /**
-     * 添加接收对象的处理对象。
-     * 
-     * @param cls 待处理的对象，其所属的类。
-     * @param action 处理过程对象。
-     */
-    public void addActionMap(Class<Object> cls, ObjectAction action) {
-        actionMapping.put(cls, action);
-    }
+	/**
+	 * 添加接收对象的处理对象。
+	 * 
+	 * @param cls
+	 *            待处理的对象，其所属的类。
+	 * @param action
+	 *            处理过程对象。
+	 */
+	public void addActionMap(Class<Object> cls, ObjectAction action) {
+		actionMapping.put(cls, action);
+	}
 
-    /**
-     * 处理服务端发回的对象，可实现该接口。
-     */
-    public static interface ObjectAction {
-        void doAction(Object obj, Client2 client);
-    }
+	/**
+	 * 处理服务端发回的对象，可实现该接口。
+	 */
+	public static interface ObjectAction {
+		void doAction(Object obj, Client2 client);
+	}
 
-    public static final class DefaultObjectAction implements ObjectAction {
-        public void doAction(Object obj, Client2 client) {
-            System.out.println("处理：\t" + obj.toString());
-        }
-    }
+	public static final class DefaultObjectAction implements ObjectAction {
+		public void doAction(Object obj, Client2 client) {
+			System.out.println("处理：\t" + obj.toString());
+		}
+	}
 
-    public void sendObject(Object obj) throws IOException {
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        oos.writeObject(obj);
-        System.out.println("发送：\t" + obj);
-        oos.flush();
-    }
+	public void sendObject(Object obj) throws IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+		oos.writeObject(obj);
+		System.out.println("发送：\t" + obj);
+		oos.flush();
+	}
 
-    /**
-     * 发送给服务端消息
-     * 
-     * @Copyright Copyright (c) 2012 - 2100
-     * @create at 2014年7月30日
-     */
-    class KeepAliveWatchDog implements Runnable {
-        long checkDelay = 10;
-        long keepAliveDelay = 2000;
+	/**
+	 * 发送给服务端消息
+	 * 
+	 * @Copyright Copyright (c) 2012 - 2100
+	 * @create at 2014年7月30日
+	 */
+	class KeepAliveWatchDog implements Runnable {
+		long checkDelay = 10;
+		long keepAliveDelay = 2000;
 
-        public void run() {
-            while (running) {
-                if (System.currentTimeMillis() - lastSendTime > keepAliveDelay) {
-                    try {
-                        Client2.this.sendObject(new KeepAlive());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Client2.this.stop();
-                    }
-                    lastSendTime = System.currentTimeMillis();
-                } else {
-                    try {
-                        Thread.sleep(checkDelay);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        Client2.this.stop();
-                    }
-                }
-            }
-        }
-    }
+		public void run() {
+			while (running) {
+				if (System.currentTimeMillis() - lastSendTime > keepAliveDelay) {
+					try {
+						Client2.this.sendObject(new KeepAlive());
+					} catch (IOException e) {
+						e.printStackTrace();
+						Client2.this.stop();
+					}
+					lastSendTime = System.currentTimeMillis();
+				} else {
+					try {
+						Thread.sleep(checkDelay);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+						Client2.this.stop();
+					}
+				}
+			}
+		}
+	}
 
-    /**
-     * 接收来自服务端消息
-     * 
-     * @Copyright Copyright (c) 2012 - 2100
-     * @create at 2014年7月30日
-     */
-    class ReceiveWatchDog implements Runnable {
-        public void run() {
-            while (running) {
-                try {
-                    InputStream in = socket.getInputStream();
-                    if (in.available() > 0) {
-                        ObjectInputStream ois = new ObjectInputStream(in);
-                        Object obj = ois.readObject();
-                        System.out.println("接收：\t" + obj);
-                        ObjectAction oa = actionMapping.get(obj.getClass());
+	/**
+	 * 接收来自服务端消息
+	 * 
+	 * @Copyright Copyright (c) 2012 - 2100
+	 * @create at 2014年7月30日
+	 */
+	class ReceiveWatchDog implements Runnable {
+		public void run() {
+			while (running) {
+				try {
+					InputStream in = socket.getInputStream();
+					if (in.available() > 0) {
+						ObjectInputStream ois = new ObjectInputStream(in);
+						Object obj = ois.readObject();
+						System.out.println("接收：\t" + obj);
+						ObjectAction oa = actionMapping.get(obj.getClass());
 
-                        Message message = new Message();
-                        message.obj = obj.toString();
-                        mHandler.sendMessage(message);
-                        oa = oa == null ? new DefaultObjectAction() : oa;
-                        oa.doAction(obj, Client2.this);
-                    } else {
-                        Thread.sleep(10);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Client2.this.stop();
-                }
-            }
-        }
-    }
+						Message message = new Message();
+						message.obj = obj.toString();
+						mHandler.sendMessage(message);
+						oa = oa == null ? new DefaultObjectAction() : oa;
+						oa.doAction(obj, Client2.this);
+					} else {
+						Thread.sleep(10);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					Client2.this.stop();
+				}
+			}
+		}
+	}
 
-    // public static void main(String[] args) throws UnknownHostException,
-    // IOException {
-    // String serverIp = "127.0.0.1";
-    // int port = 65432;
-    // Client2 client = new Client2(serverIp, port);
-    // client.start();
-    // }
+	// public static void main(String[] args) throws UnknownHostException,
+	// IOException {
+	// String serverIp = "127.0.0.1";
+	// int port = 65432;
+	// Client2 client = new Client2(serverIp, port);
+	// client.start();
+	// }
 }
--- src/com/example/socket/MainActivity.java
+++ src/com/example/socket/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.socket;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -8,28 +7,19 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.security.PublicKey;
 import java.util.Enumeration;
 
 import org.apache.http.conn.util.InetAddressUtils;
 
-import com.example.socket.PServer2.ObjectAction;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
-import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -37,204 +27,206 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.socket.PServer2.ObjectAction;
+
 public class MainActivity extends Activity implements ObjectAction {
 
-    private TextView txtvs;// 显示连接信息
-    private Button btnConnect, btnServer, btnSend;// server按钮
-    private String IPAddress;
-    private EditText edit_send;
-    private Button btn_send;
-    Client2 client;
-    PServer2 server;
-    boolean isServer = false;
-    public Handler mHandler = new Handler() {
+	private TextView txtvs;// 显示连接信息
+	private Button btnConnect, btnServer, btnSend;// server按钮
+	private String IPAddress;
+	private EditText edit_send;
+	private Button btn_send;
+	Client2 client;
+	PServer2 server;
+	boolean isServer = false;
+	public Handler mHandler = new Handler() {
 
-        @Override
-        public void handleMessage(android.os.Message msg) {
-            if (msg.what == 1) {
-                txtvs.setText("连接成功！");
-            } else {
-                String str = msg.obj.toString();
-                if (!isDate(str)) {
-                    txtvs.setText("接收到消息：" + str);
-                }
-            }
-        };
-    };
+		@Override
+		public void handleMessage(android.os.Message msg) {
+			if (msg.what == 1) {
+				txtvs.setText("连接成功！");
+			} else {
+				String str = msg.obj.toString();
+				if (!isDate(str)) {
+					txtvs.setText("接收到消息：" + str);
+				}
+			}
+		};
+	};
 
-    public boolean isDate(String str) {
-        java.text.SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        try {
-            format.parse(str);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
+	public boolean isDate(String str) {
+		java.text.SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			format.parse(str);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
 
-    @SuppressLint("NewApi")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        inintView();
-        inintListener();
-    }
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		inintView();
+		inintListener();
+	}
 
-    private void inintView() {
-        // TODO Auto-generated method stub
-        edit_send = (EditText) findViewById(R.id.edit_send);
-        btn_send = (Button) findViewById(R.id.btn_send);
+	private void inintView() {
+		// TODO Auto-generated method stub
+		edit_send = (EditText) findViewById(R.id.edit_send);
+		btn_send = (Button) findViewById(R.id.btn_send);
 
-        btnConnect = (Button) findViewById(R.id.connect);
-        btnSend = (Button) findViewById(R.id.btn_send);
-        txtvs = (TextView) findViewById(R.id.textView1);
-        IPAddress = getlocalip();
-        if (IPAddress == null) {
-            IPAddress = getLocalIpAddress();
-        }
-        txtvs.setText("本地WIFI-IP地址:" + IPAddress);// 初始显示本地IP
-        btnServer = (Button) findViewById(R.id.server);
-        if (IPAddress.split("\\.")[3].equals("1")) {
-            openService();
-        }
-    }
+		btnConnect = (Button) findViewById(R.id.connect);
+		btnSend = (Button) findViewById(R.id.btn_send);
+		txtvs = (TextView) findViewById(R.id.textView1);
+		IPAddress = getlocalip();
+		if (IPAddress == null) {
+			IPAddress = getLocalIpAddress();
+		}
+		txtvs.setText("本地WIFI-IP地址:" + IPAddress);// 初始显示本地IP
+		btnServer = (Button) findViewById(R.id.server);
+		if (IPAddress.split("\\.")[3].equals("1")) {
+			openService();
+		}
+	}
 
-    private void openService() {
-        isServer = true;
-        btnConnect.setEnabled(false);
-        int port = 65432;
-        server = new PServer2(MainActivity.this, port, mHandler);
-        server.start();
-    }
+	private void openService() {
+		isServer = true;
+		btnConnect.setEnabled(false);
+		int port = 65432;
+		server = new PServer2(MainActivity.this, port, mHandler);
+		server.start();
+	}
 
-    private void inintListener() {
-        /**
-         * SERVER端事件
-         */
-        btnServer.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                openService();
-            }
+	private void inintListener() {
+		/**
+		 * SERVER端事件
+		 */
+		btnServer.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				openService();
+			}
 
-        });
-        /**
-         * client端事件
-         */
-        btnConnect.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                btnServer.setEnabled(false);
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
+		});
+		/**
+		 * client端事件
+		 */
+		btnConnect.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				btnServer.setEnabled(false);
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
 
-                        try {
-                            String serverIp = "192.168.1.1";
-                            int port = 65432;
-                            client = new Client2(mHandler, serverIp, port);
-                            client.start();
-                        } catch (UnknownHostException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
-            }
+						try {
+							String serverIp = "192.168.1.1";
+							int port = 65432;
+							client = new Client2(mHandler, serverIp, port);
+							client.start();
+						} catch (UnknownHostException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}).start();
+			}
 
-        });
-        btn_send.setOnClickListener(new OnClickListener() {
+		});
+		btn_send.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                try {
-                    Object objs = edit_send.getText().toString();
-                    txtvs.setText("发送<" + edit_send.getText().toString() + ">成功>");
-                    OutputStream op = null;
-                    if (isServer) {
-                        if (server.socket.isConnected()) {
-                            op = server.socket.getOutputStream();
-                        } else {
-                            Toast.makeText(MainActivity.this, "连接已断开", 2000).show();
-                        }
-                    } else {
-                        if (client.socket.isConnected()) {
-                            op = client.socket.getOutputStream();
-                        } else {
-                            Toast.makeText(MainActivity.this, "连接已断开", 2000).show();
-                        }
-                    }
-                    ObjectOutputStream oop = new ObjectOutputStream(op);
-                    oop.writeObject(objs);
-                    System.out.println("发送!!!!!!!!!!：\t" + objs);
-                    oop.flush();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+			@Override
+			public void onClick(View v) {
+				try {
+					Object objs = edit_send.getText().toString();
+					txtvs.setText("发送<" + edit_send.getText().toString() + ">成功>");
+					OutputStream op = null;
+					if (isServer) {
+						if (server.socket.isConnected()) {
+							op = server.socket.getOutputStream();
+						} else {
+							Toast.makeText(MainActivity.this, "连接已断开", 2000).show();
+						}
+					} else {
+						if (client.socket.isConnected()) {
+							op = client.socket.getOutputStream();
+						} else {
+							Toast.makeText(MainActivity.this, "连接已断开", 2000).show();
+						}
+					}
+					ObjectOutputStream oop = new ObjectOutputStream(op);
+					oop.writeObject(objs);
+					System.out.println("发送!!!!!!!!!!：\t" + objs);
+					oop.flush();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    /**
-     * 通过wifi连接获取IP地址
-     * 
-     * @return
-     */
-    private String getlocalip() {
-        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-        int ipAddress = wifiInfo.getIpAddress();
-        // Log.d(Tag, "int ip "+ipAddress);
-        System.out.println(ipAddress);
-        if (ipAddress == 0)
-            return null;
-        return ((ipAddress & 0xff) + "." + (ipAddress >> 8 & 0xff) + "." + (ipAddress >> 16 & 0xff) + "." + (ipAddress >> 24 & 0xff));
-    }
+	/**
+	 * 通过wifi连接获取IP地址
+	 * 
+	 * @return
+	 */
+	private String getlocalip() {
+		WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+		int ipAddress = wifiInfo.getIpAddress();
+		// Log.d(Tag, "int ip "+ipAddress);
+		System.out.println(ipAddress);
+		if (ipAddress == 0)
+			return null;
+		return ((ipAddress & 0xff) + "." + (ipAddress >> 8 & 0xff) + "." + (ipAddress >> 16 & 0xff) + "." + (ipAddress >> 24 & 0xff));
+	}
 
-    /**
-     * GPRS地址
-     */
-    public String getLocalIpAddress() {
-        // ----------------------
-        String ipaddress = "";
-        try {
-            Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
-            // 遍历所用的网络接口
-            while (en.hasMoreElements()) {
-                NetworkInterface nif = en.nextElement();// 得到每一个网络接口绑定的所有ip
-                Enumeration<InetAddress> inet = nif.getInetAddresses();
-                // 遍历每一个接口绑定的所有ip
-                while (inet.hasMoreElements()) {
-                    InetAddress ip = inet.nextElement();
-                    if (!ip.isLoopbackAddress() && InetAddressUtils.isIPv4Address(ip.getHostAddress())) {
-                        ipaddress = ip.getHostAddress();
-                        return ipaddress;
-                    }
-                }
+	/**
+	 * GPRS地址
+	 */
+	public String getLocalIpAddress() {
+		// ----------------------
+		String ipaddress = "";
+		try {
+			Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+			// 遍历所用的网络接口
+			while (en.hasMoreElements()) {
+				NetworkInterface nif = en.nextElement();// 得到每一个网络接口绑定的所有ip
+				Enumeration<InetAddress> inet = nif.getInetAddresses();
+				// 遍历每一个接口绑定的所有ip
+				while (inet.hasMoreElements()) {
+					InetAddress ip = inet.nextElement();
+					if (!ip.isLoopbackAddress() && InetAddressUtils.isIPv4Address(ip.getHostAddress())) {
+						ipaddress = ip.getHostAddress();
+						return ipaddress;
+					}
+				}
 
-            }
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-        return ipaddress;
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		return ipaddress;
 
-    }
+	}
 
-    @Override
-    public Object doAction(Activity context, Object rev) {
-        // TODO Auto-generated method stub
-        System.out.println("main处理并返回：" + rev);
-        if (rev.toString().equals("10010")) {
-            Uri uri = Uri.parse("tel:10010");
-            Intent it = new Intent(Intent.ACTION_CALL, uri);
-            context.startActivity(it);
-        }
-        return rev;
-    }
+	@Override
+	public Object doAction(Activity context, Object rev) {
+		// TODO Auto-generated method stub
+		System.out.println("main处理并返回：" + rev);
+		if (rev.toString().equals("10010")) {
+			Uri uri = Uri.parse("tel:10010");
+			Intent it = new Intent(Intent.ACTION_CALL, uri);
+			context.startActivity(it);
+		}
+		return rev;
+	}
 
 }
